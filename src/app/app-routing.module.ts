10937e72886758dbce0aea923bd2bcedf9f24f14
danny <dannyzambranasanabria@gmail.com>
2020-10-08 13:34:58 -0400

    pantalla nosotros y contactanos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { CulturalComponent } from './cultural/cultural.component';
+import { GastronomicoComponent } from './gastronomico/gastronomico.component';
+const Routes: Routes = [
+  {
+    path: 'Cultural',
+    component: CulturalComponent,
+  },
+  {
+    path: 'Gastronomico',
+    component: GastronomicoComponent,
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent,
+  },
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(Routes)],
+exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CulturalComponent } from './cultural/cultural.component';
 import { GastronomicoComponent } from './gastronomico/gastronomico.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CulturalComponent,
-    GastronomicoComponent
+    GastronomicoComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
