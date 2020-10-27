89f67008775310ff2f9efc4ba87a9782c492442e
FerchitaSM <45465803+FerchitaSM@users.noreply.github.com>
2020-10-27 14:41:51 -0400

    Rutas al fin

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone, OnDestroy, Output, Input, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { ServicioService } from '../servicio.service';
 import { Place } from '../place';
+import { FormControl } from '@angular/forms';
+import { AgmMap, MapsAPILoader } from '@agm/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-detalle',
@@ -12,9 +15,12 @@ export class DetalleComponent implements OnInit {
   place= new Place;
   mylat: number;
   mylng: number;
-  zoom: number = 15;
+  zoom: number = 18;
   mapTyoeId: string = 'hybrid';
-  
+  origin:any;
+  destination:any;
+  look: boolean = false;
+
   constructor(private service:ServicioService, private router: Router) { }
 
   ngOnInit(): void {
@@ -28,9 +34,10 @@ export class DetalleComponent implements OnInit {
     navigator.geolocation.getCurrentPosition(position =>{
       this.mylat =position.coords.latitude;
       this.mylng =position.coords.longitude;
-      this.zoom =12;
+      this.origin ={lat: this.mylat, lng: this.mylng};
+      this.destination = {lat: this.place.latitud , lng: this.place.longitud};
+      this.look = true;
     })
-
   }
 
 }
